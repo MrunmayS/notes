7e3adee8759a70ecb4d74213503c66511f5c0475
Mrunmay Shelar <51090437+MrunmayS@users.noreply.github.com>
2021-02-04 21:11:10 +0530

    Day 24 complete

--- MRUNMAY/learning-python/Day 24/Imporved-snake-game/score.py
+++ MRUNMAY/learning-python/Day 24/Imporved-snake-game/score.py
@@ -6,6 +6,8 @@ class Score(Turtle):
         super().__init__()
         self.color('white')
         self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.goto(0,270)
         self.hideturtle()
@@ -23,6 +25,8 @@ class Score(Turtle):
     def reset(self):
         if self.points > self.high_score:
             self.high_score = self.points
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.points = 0
         self.upated_score()
     
