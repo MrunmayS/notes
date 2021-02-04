831ceca7822519f861983875d12f8aaf1f1816b6
Mrunmay Shelar <51090437+MrunmayS@users.noreply.github.com>
2021-02-04 21:07:17 +0530

    Day 22-24

---
+++ MRUNMAY/learning-python/Day 24/Imporved-snake-game/score.py
@@ -0,0 +1,31 @@
+from turtle import Turtle
+
+class Score(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color('white')
+        self.high_score = 0
+        self.penup()
+        self.goto(0,270)
+        self.hideturtle()
+        self.points = 0 
+        self.upated_score()
+
+    def upated_score(self):
+        self.clear()
+        self.write(f"Score: {self.points} High Score: {self.high_score}", align='center', font=("Courier", 24,"normal"))
+
+    def add_points(self):
+        self.points += 1
+        self.upated_score()
+
+    def reset(self):
+        if self.points > self.high_score:
+            self.high_score = self.points
+        self.points = 0
+        self.upated_score()
+    
+    '''def game_over(self):
+        self.goto(0,0)
+        self.write("Game Over", align='center', font=("Courier", 24,"normal"))'''
